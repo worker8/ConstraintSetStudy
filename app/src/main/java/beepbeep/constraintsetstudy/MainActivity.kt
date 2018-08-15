b23367f343cba3630f55f8dd5ca1f29d34034a1f
Tan Jun Rong <tanjunrong@gmail.com>
2018-08-15 22:30:20 +0900

    add comments

--- app/src/main/java/beepbeep/constraintsetstudy/MainActivity.kt
+++ app/src/main/java/beepbeep/constraintsetstudy/MainActivity.kt
@@ -11,15 +11,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 1. Inflate the TextView row & add the the TextView
         val textViewRow = LayoutInflater.from(this).inflate(R.layout.text_view_row, constraintLayout, false)
         constraintLayout.addView(textViewRow)
 
+        // 2. Clone the constraints into ConstraintSet
         val set = ConstraintSet()
         set.clone(constraintLayout)
 
+        // 3. Connect TextView row with ConstraintLayout
         set.connect(textViewRow.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
         set.connect(textViewRow.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START)
         set.connect(textViewRow.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END)
+
+        // 4. applying the constraint
         set.applyTo(constraintLayout)
     }
 }
